24444a3f7126240e42d57927405c6e7d44e164c7
Michal Rubajczyk <rubajticos@gmail.com>
2022-10-22 13:28:42 +0200

    Trying to crash Firebase on update multiple paths

--- app/src/main/java/pl/rubajticos/pepfirebase/ui/screen/MainViewModel.kt
+++ app/src/main/java/pl/rubajticos/pepfirebase/ui/screen/MainViewModel.kt
@@ -5,10 +5,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.io.File
+import java.lang.Byte
 import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import pl.rubajticos.pepfirebase.data.PeopleRepository
 import pl.rubajticos.pepfirebase.data.RealtimeBookRepository
 import timber.log.Timber
@@ -16,7 +21,8 @@ import timber.log.Timber
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val peopleRepository: PeopleRepository,
-    private val booksRepository: RealtimeBookRepository
+    private val booksRepository: RealtimeBookRepository,
+    private val database: FirebaseDatabase
 ) : ViewModel() {
 
     var state by mutableStateOf(BooksState())
@@ -40,6 +46,31 @@ class MainViewModel @Inject constructor(
 
             val token = task.result
             Timber.d("MRMR FCM token $token")
+        }
+
+        trySendUpdate()
+    }
+
+    private fun trySendUpdate() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val ref = database.reference
+            val update: Map<String, Any> = mapOf(
+                "test/1/name" to "Michael",
+                "test/1/lastname" to "Scott",
+                "error/1/" to listOf(Byte.valueOf(1))
+            )
+
+            runCatching {
+                ref.updateChildren(update)
+                    .addOnCompleteListener { updateTask ->
+                        if (updateTask.isSuccessful) {
+                            Timber.d("MRMR Update success")
+                        } else {
+                            Timber.e("MRMR ${updateTask.exception?.localizedMessage ?: "Error"}")
+                        }
+                    }
+            }
+                .onFailure { Timber.e("MRMR error -> ${it.localizedMessage}") }
         }
     }
 
